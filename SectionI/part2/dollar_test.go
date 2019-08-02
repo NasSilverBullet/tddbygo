ad62c0bb990035a90b1aeae3ae2c6816f84d2327
y.t <y.softvalley@gmail.com>
2019-08-02 11:55:48 +0900

    fix test

--- SectionI/part2/dollar_test.go
+++ SectionI/part2/dollar_test.go
@@ -3,14 +3,14 @@ package dollar
 import "testing"
 
 func TestMultiplication(t *testing.T) {
-	five := Dollar{amount: 5}
+	five := &Dollar{amount: 5}
 	product := five.Times(2)
 	if expected, actual := 10, product.amount; expected != actual {
-		t.Errorf("five.Times(2).amount wont %d but got %d", expected, actual)
+		t.Errorf("&Dollar{amount: 5}.Times(2).amount wont %d but got %d", expected, actual)
 	}
 	product = five.Times(3)
 	if expected, actual := 15, product.amount; expected != actual {
-		t.Errorf("five.Times(3).amount wont %d but got %d", expected, actual)
+		t.Errorf("&Dollar{amount: 5}.Times(3).amount wont %d but got %d", expected, actual)
 	}
 
 }
--- SectionI/part3/dollar_test.go
+++ SectionI/part3/dollar_test.go
@@ -6,11 +6,11 @@ func TestMultiplication(t *testing.T) {
 	five := &Dollar{amount: 5}
 	product := five.Times(2)
 	if expected, actual := 10, product.amount; expected != actual {
-		t.Errorf("five.Times(2).amount wont %d but got %d", expected, actual)
+		t.Errorf("&Dollar{amount: 5}.Times(2).amount wont %d but got %d", expected, actual)
 	}
 	product = five.Times(3)
 	if expected, actual := 15, product.amount; expected != actual {
-		t.Errorf("five.Times(2).amount wont %d but got %d", expected, actual)
+		t.Errorf("&Dollar{amount: 5}.Times(3).amount wont %d but got %d", expected, actual)
 	}
 
 }
